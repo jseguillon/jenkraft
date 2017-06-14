eccdb011cef4184d9af45682ea428a1868b7311f
jseguillon <joel.seguillon@gmail.com>
2017-06-14 13:00:20 +0200

    Fancy graphics + more jobs statuses handled + autowalk + some of refactor and docs

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# WIP
+# jenkraft.py
+
+jenkraft.py is jenkins REST exploring for Minecraft interraction
+
+## Start
+
+Launch python2 `python` then `execfile('jenkraft.py')`
+
+## Configuration
+
+TODO yaml config
+
+# WIP Zone
+
+## A fountain
+
+TODO : ascii schema for more comprehensive
 
 ## Notes for developping
 
@@ -30,9 +46,10 @@ f.clear()
 => -24,-12,0,12,24
 => -40, -30, -10, 0, 10, 20, 30, 40, 50
 
-### Materials that should be used
+### Materials that could be used as base for fountains
 
-GRASS, DIRT, COBBLESTONE, DIAMOND_ORE, STONE_BRICK, GOLD_ORE, IRON_ORE, MUSHROOM_BROWN, FLOWER_YELLOW, SAND
+GRASS, DIRT, COBBLESTONE, DIAMOND_ORE, STONE_BRICK, GOLD_ORE, IRON_ORE, GOLD_BLOCK, IRON_BLOCK, STONE_SLAB_DOUBLE, STONE_SLAB, BRICK_BLOCK, MOSS_STONE, DIAMOND_BLOCK, LAPIS_LAZULI_ORE, LAPIS_LAZULI_BLOCK, SANDSTONE, COBWEB, GRASS_TALL, REDSTONE_ORE, GLOWSTONE_BLOCK
+
 
 ### Other usefull commands
 reload(jenkraft)
--- jenkraft.py
+++ jenkraft.py
@@ -18,10 +18,12 @@ JOB_URL="http://192.168.99.100:8080/job/jenkraft/wfapi/runs"
 JOB_AUTH=('jse', 'jse')
 
 JOB_RUNNING="IN_PROGRESS"
+JOB_NOT_EXECUTED="NOT_EXECUTED"
 JOB_SUCCESS="SUCCESS"
+JOB_UNSTABLE="UNSTABLE"
 JOB_FAILED="FAILED"
 JOB_ABORTED="ABORTED"
-JOB_REFRESH_TIME=10 #Refresh each 60s
+JOB_REFRESH_TIME=30 #Refresh each 30s
 
 MAX_OCEAN_Y=5
 MAX_OCEAN_X=64
@@ -40,31 +42,50 @@ DEFAULT_BRANCH_START_Y=15
 class Scene:
     def __init__(self, mc):
         self.mc = mc
-        self.jobs = {}
-        ocean = Ocean(1, mc)
+        self.jobs = []
 
-        job  = Job(1, JOB_URL, JOB_AUTH, block.DIAMOND_ORE, mc)
-        job.start()
-        self.jobs[1] = job
+        #TODO yml config (+config per branch alley height (or dynamic walk height ? ))
+        for i, m, url, auth in [ (0, block.GRASS, "https://ci.jenkins.io/job/Plugins/job/git-plugin/job/PR-493/wfapi/runs", None),
+            (1, block.COBBLESTONE, "https://ci.jenkins.io/job/Infra/job/backend-extension-indexer/job/PR-26/wfapi/runs", None),
+            (2, block.IRON_ORE, "https://ci.jenkins.io/job/Plugins/job/git-plugin/view/change-requests/job/PR-375/wfapi/runs", None),
+            (3, block.DIAMOND_ORE, "https://ci.jenkins.io/job/Infra/job/backend-extension-indexer/job/PR-26/wfapi/runs", None) ]:
+            #job  = Job(i, JOB_URL, JOB_AUTH, m, mc)
+            job = Job(i, url, auth, m, self.mc)
+            job.start()
+            self.jobs.append(job)
+            time.sleep(2)
 
+        self.do_loop = True
         self.loop()
 
     def loop(self):
-        #Start Infinitz loop threaded branches
-        #Parse branches and ensure all last job on same x ?
         i=0
-        while True:
-            #for job in self.jobs:
-            i += 1
-            x = -70+(i)
-            mc.player.setPos(x,DEFAULT_POS_Y,DEFAULT_POS_Z)
-            mc.setBlock(x,DEFAULT_POS_Y-1,DEFAULT_POS_Z, block.BEDROCK_INVISIBLE)
+        while self.do_loop is True:
+            for job in self.jobs:
+                print("job watching {}".format(job.id))
+                z = DEFAULT_POS_Z + job.id*12 + 4
+                exit_job = False
+                i=0
+                while self.do_loop is True and exit_job is False:
+                    i += 1
+                    x = -70+(i) #TODO : more than 6 latest builds is useless, pause earlyer, walk slowly
 
-            #TODO 12*job_id, add wool for last five build ? loop only one 
-            if x >64:
-                i = 1
+                    #Don't fall
+                    #TODO : might better draw this while drawing fountains ?
+                    mc.setBlocks(x-1,DEFAULT_POS_Y-1,z-1, x+1,DEFAULT_POS_Y-1,z+1, block.GLASS)
+                    time.sleep(0.1)
+                    mc.player.setPos(x,DEFAULT_POS_Y,z)
 
-            time.sleep(1+(random.randint(0,10)/10))
+                    #FIXME: this is not a good pattern
+                    time.sleep(1+(random.randint(0,10)/10))
+
+                    if (x>=(-70 + 10*len(job.fountains))):
+                        exit_job = True
+                #Sleep more before changing job
+                time.sleep(10)
+
+    def stop_loop(self):
+        self.do_loop = False
 
 class Job(threading.Thread):
     """Jobs : can be a branch or a simple job, only URL is known by code """
@@ -74,79 +95,83 @@ class Job(threading.Thread):
         self.setDaemon(True)
         self.id = id
         self.mc = mc
+        self.url = url
         self.material=material
+        #TODO : would be clearer to have a build list instead of fountain keyword
         self.fountains = {} #int id: (status: , stages: , f:)
 
     def run(self):
         while True :
-        #TODO : get list of jobs, with number of stages and status
-            print("[job-{}] Discovering".format(self.id))
-            #FIXME : Try catch is needed there to ensure jobs keep polling
-            ##stages=response = requests.get(sefl.url,  auth=JOB_AUTH)
-            with open('jenkraft.example.json') as data_file:
-                data = json.load(data_file)
+            self.collect()
+            self.draw()
+            print("end : sleeping")
+            time.sleep(JOB_REFRESH_TIME)
 
-            job_count = 0
-            for job_data in data:
-                stagesCount = 0
-                job_id = int(job_data['id'])
-                job_status = job_data['status']
+    def collect(self):
+        print("[job-{}] Discovering".format(self.id))
+        #FIXME : Try catch is needed there to ensure jobs keep polling
+        response = requests.get(self.url)
+        data = json.loads(response.text)
+        #fjson = 'jenkraft.example.{}.json'.format((self.id%2))
+        #with open(response) as data_file:
+        #    data = json.load(data_file)
 
-                print(job_data['id']+" : "+job_data['status'])
+        job_count = 0
+        for job_data in data:
+            stagesCount = 0
+            job_id = int(job_data['id'])
+            job_status = job_data['status']
 
-                #Because of declarative pipelines we loop over all stages
-                #If any of the stages is different from SUCCESS then stop couting
-                #Because we want to stop and consider only stages that really ran
-                for stage in job_data['stages']:
-                    stagesCount = stagesCount + 1
-                    print(" - stage {} status : {} ".format(stage['id'], stage['status']))
-                    if stage['status'] != "SUCCESS":
-                        break
-                print (" - stages count {} ".format(stagesCount))
+            print(job_data['id']+" : "+job_data['status'])
 
-                if (job_id in self.fountains):
-                    print ("updating build : {}".format(job_id))
-                    self.fountains[job_id]['stages'] = stagesCount
-                    self.fountains[job_id]['status'] = job_status
+            #Because of declarative pipelines we loop over all stages
+            #If any of the stages is different from SUCCESS then stop couting
+            #Because we want to stop and consider only stages that really ran
+            for stage in job_data['stages']:
+                stagesCount = stagesCount + 1
+                if stage['status'] != "SUCCESS":
+                    break
+            print (" - stages count {} ".format(stagesCount))
 
-                else:
-                    print ("creating build : {}".format(job_id))
-                    self.fountains[job_id] = {'stages': stagesCount, 'status': job_status, 'f': None}
+            if (job_id in self.fountains):
+                print ("updating build : {}".format(job_id))
+                self.fountains[job_id]['stages'] = stagesCount
+                self.fountains[job_id]['status'] = job_status
+            else:
+                print ("creating build : {}".format(job_id))
+                self.fountains[job_id] = {'stages': stagesCount, 'status': job_status, 'f': None}
 
             print("now draw with ordered list")
 
-            sorted_jobs = collections.OrderedDict(sorted(self.fountains.iteritems(), key=lambda x: x[0]))
-            for job_id in sorted_jobs:
-                #TODO (FEATURE) : use a type of encoding for remembering id ?
+    def draw(self):
+        sorted_jobs = collections.OrderedDict(sorted(self.fountains.iteritems(), key=lambda x: x[0]))
+        for job_id in sorted_jobs:
+            #FEATURE : use a type of encoding for remembering id, commiter colour ?
 
-                if self.fountains[job_id]['f'] is not None:
-                    print ("fountain already referenced : {}".format(job_id))
+            if self.fountains[job_id]['f'] is not None:
+                print ("fountain already referenced : {}".format(job_id))
 
-                else:
-                    print("Creating new fountain {}, {}, {}, {}".format(
+            else:
+                print("Creating new fountain {}, {}, {}, {}".format(
+                -64+(sorted_jobs.keys().index(job_id)*12),
+                    MAX_OCEAN_Y,
+                    -(MAX_OCEAN_Z-10)+self.id*12, self.material))
+
+                f = Fountain(1, self.mc,
                     -64+(sorted_jobs.keys().index(job_id)*12),
-                        MAX_OCEAN_Y,
-                        -(MAX_OCEAN_Z-10), self.material))
-                    #TODO : 60=branche relative index, other step=12, step= 10
-                    f = Fountain(1, self.mc,
-                        -64+(sorted_jobs.keys().index(job_id)*12),
-                        MAX_OCEAN_Y,
-                        -(MAX_OCEAN_Z-10), self.material)
+                    MAX_OCEAN_Y,
+                    -(MAX_OCEAN_Z-10)+self.id*12, self.material)
 
-                    f.draw_flow()
-                    self.fountains[job_id]['f'] = f
+                self.fountains[job_id]['f'] = f
+                time.sleep(10)
+            f = self.fountains[job_id]['f']
+            #FIXME: should not changes representation if nor stages nor status is changed
+            #Plus TODO : if something changed, would be good to teleport to position before rendering (pause loop, teleport, sleep 10, loop again)
+            f.set_stages(self.fountains[job_id]['stages'])
 
-                f = self.fountains[job_id]['f']
-                #//TODO : on finish : met them flow
-                f.set_stages(self.fountains[job_id]['stages'])
-                f.set_status(self.fountains[job_id]['status'])
-
-                #expe : limit
-                if (job_count>99):
-                    break
-
-            print("end : sleeping")
-            time.sleep(JOB_REFRESH_TIME)
+            #FIXME: set status handles draw_flow cause of previous status handle. Refactor with switch_state ?
+            f.set_status(self.fountains[job_id]['status'])
+            #f.draw_flow()
 
     def stop(self):
         self._stop_event.set()
@@ -170,29 +195,6 @@ class Job(threading.Thread):
             f['f'].clear()
             print("clear fountains")
             #    x.clear()
-
-class Ocean:
-    def __init__(self, id, mc):
-        self.id = id
-        self.mc = mc
-        self.draw()
-
-    def __cmp__(self, rhs):
-        return hash(self) - hash(rhs)
-
-    def __hash__(self):
-        return (self.id << 8) + self.data
-
-    def draw(self):
-        #Bedrock as an ocean
-        self.mc.setBlocks(-MAX_OCEAN_X, MAX_OCEAN_Y-2, -MAX_OCEAN_Z,
-                          MAX_OCEAN_X, MAX_OCEAN_Y-5, MAX_OCEAN_Z,
-                          block.BEDROCK_INVISIBLE)
-
-        #Init scene
-        self.mc.setBlock(DEFAULT_POS_X, DEFAULT_POS_Y-1, DEFAULT_POS_Z, block.BEDROCK_INVISIBLE)
-        self.mc.player.setPos(DEFAULT_POS_X, DEFAULT_POS_Y, DEFAULT_POS_Z)
-
 
 class Fountain:
     """Jenkraft fountain"""
@@ -222,10 +224,12 @@ class Fountain:
 
     def build_base(self):
         #Blast
-        self.mc.setBlocks(self.x-6,self.y,self.z-12,
-                          self.x+6,self.y+200,self.z+12,
+        self.mc.setBlocks(self.x-7,self.y-10,self.z-7,
+                          self.x+7,self.y+50,self.z+7,
                           block.AIR)
+        time.sleep(1)  #Blast may take some time
 
+        #Recepient
         self.mc.setBlocks(self.x-2,self.y,self.z-2,
                           self.x+2,self.y,self.z+2,
                           self.material)
@@ -233,18 +237,25 @@ class Fountain:
                           self.x+1,self.y,self.z+1,
                           block.AIR)
 
+        #Base
         self.mc.setBlocks(self.x-4,self.y-1,self.z-4,
                           self.x+4,self.y-1,self.z+4,
                           self.material)
 
+        #Torches
+        for x, z in [ (2,2), (2,-2), (-2,2), (-2,-2) ]:
+            self.mc.setBlock(self.x+x,self.y+1,self.z+z, block.TORCH)
+
+        #Floor
+        self.mc.setBlocks(self.x-7,self.y-2,self.z-7,
+                          self.x+7,self.y-2,self.z+7,
+                          block.COBBLESTONE)
 
     def set_stages(self, num):
         self.height=num
-        self.draw_flow()
 
     def add_stage(self):
         self.height+=1
-        self.draw_flow()
 
     def draw_flow(self):
         #clear first in case iand give a lil of time for flowings to disapear in case of statsus change
@@ -252,17 +263,32 @@ class Fountain:
             self.mc.setBlocks(self.x-1, self.y, self.z-1, self.x+1, self.y+self.height,self.z+1,block.AIR)
             time.sleep(0.2*self.height)
 
-        block_to_build = block.WATER_FLOWING
-        if (self.status==JOB_FAILED):
-            block_to_build = block.LAVA_FLOWING
-        elif (self.status==JOB_ABORTED):
-            block_to_build = block.GLASS
-        self.mc.setBlocks(self.x, self.y, self.z, self.x, self.y+self.height,self.z,block_to_build)
+        block_to_build = block.WATER_STATIONARY
+        width=0
+        #Fill fountain execpt if not executed
+        if (self.status!=JOB_NOT_EXECUTED):
+            if (self.status==JOB_FAILED):
+                block_to_build = block.LAVA_STATIONARY
+            elif (self.status==JOB_ABORTED):
+                block_to_build = block.GLASS
+                width=1
+            self.mc.setBlocks(self.x-width, self.y, self.z-width, self.x+width, self.y+self.height,self.z+width,block_to_build)
 
-    def clear(self):
-        self.mc.setBlocks(self.x-4,self.y-1,self.z-4,
-                          self.x+4,self.y+self.height,self.z+4,
-                          block.AIR)
+        #FIXME: in case of snow : hole y +1 may block liquid
+        #if done -> let liquid flow away
+        if (self.status in (JOB_SUCCESS, JOB_FAILED, JOB_ABORTED, JOB_UNSTABLE)):
+            for x,z in [ (2,0), (-2,0), (0,2), (0,-2) ]:
+                self.mc.setBlock(self.x+x, self.y, self.z+z, block.AIR)
+
+        #Unstable builds get flammes 
+        if (self.status==JOB_UNSTABLE):
+            self.mc.setBlocks(self.x-1, self.y+self.height+2, self.z-1, self.x+1, self.y+self.height+2,self.z+1,block.FIRE)
+
+        #FIXME: at least deprecated
+        def clear(self):
+            self.mc.setBlocks(self.x-4,self.y-1,self.z-4,
+                              self.x+4,self.y+self.height,self.z+4,
+                              block.AIR)
 
     def set_status(self, status):
         print(" status {}".format(status))
